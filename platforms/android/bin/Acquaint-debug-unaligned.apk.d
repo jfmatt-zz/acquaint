35978f29a3f1f80ae111a6624a919a55d3ac7733
louisfettet <louisfettet@gmail.com>
2014-04-05 16:46:56 -0400

    Cached those pesky bin files

--- platforms/android/bin/Acquaint-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/louis/Github/acquaint/platforms/android/bin/Acquaint-debug-unaligned.apk : \
-/home/louis/Github/acquaint/platforms/android/bin/Acquaint.ap_ \
-/home/louis/Github/acquaint/platforms/android/bin/classes.dex \
-/home/louis/Github/acquaint/platforms/android/libs/cordova-3.0.0.jar \
--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/louis/Github/acquaint/platforms/android/bin/AndroidManifest.xml : \
-/home/louis/Github/acquaint/platforms/android/AndroidManifest.xml \
