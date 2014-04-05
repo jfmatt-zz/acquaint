3ad8613e04e4f6a0c19285a680e5c44edcb2eacd
James Matthews <jfmatt8067@gmail.com>
2014-04-05 12:09:31 -0400

    Remove bin folder

--- platforms/android/bin/Acquaint-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/james/Projects/acquaint/platforms/android/bin/Acquaint-debug-unaligned.apk : \
-/home/james/Projects/acquaint/platforms/android/bin/Acquaint.ap_ \
-/home/james/Projects/acquaint/platforms/android/bin/classes.dex \
-/home/james/Projects/acquaint/platforms/android/libs/cordova-3.0.0.jar \
--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/james/Projects/acquaint/platforms/android/bin/AndroidManifest.xml : \
-/home/james/Projects/acquaint/platforms/android/AndroidManifest.xml \
